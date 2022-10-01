70e72ae6aedb8a61b8c35d2fa7b1ed222d62007c
ChrisChan888 <oscaryiu.lapsang@gmail.com>
2022-10-01 22:19:04 +0800

    feat: update title

--- src/pages/exercises/ex1.tsx
+++ src/pages/exercises/ex1.tsx
@@ -3,11 +3,13 @@ import * as React from 'react';
 import ExerciseLayout from '@/components/common/ExerciseLayout';
 import { ProductPreviewCard } from '@/components/ex1/ProductPreviewCard';
 
+import { exerciseSummarys } from '@/constant/exerciseSummary';
+
 export default function Ex1Page() {
   return (
     <ExerciseLayout
       className='flex h-screen w-screen items-center justify-center bg-ex1-cream'
-      templateTitle='Product Preview Card Component'
+      templateTitle={exerciseSummarys.ex1.title}
     >
       <ProductPreviewCard />
     </ExerciseLayout>
--- src/pages/exercises/ex2.tsx
+++ src/pages/exercises/ex2.tsx
@@ -3,10 +3,12 @@ import React from 'react';
 import ExerciseLayout from '@/components/common/ExerciseLayout';
 import QrCodeComponent from '@/components/ex2/QRCodeComponent';
 
+import { exerciseSummarys } from '@/constant/exerciseSummary';
+
 export default function Ex2Page() {
   return (
     <ExerciseLayout
-      templateTitle='QrCode Component'
+      templateTitle={exerciseSummarys.ex2.title}
       className='flex h-screen w-screen items-center justify-center bg-ex2-lightGrey'
     >
       <QrCodeComponent />
